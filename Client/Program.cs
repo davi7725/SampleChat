560822ac89bd786034c9df9e37d403910a2eb002
davi7725 <davi7725@edu.eal.dk>
2017-08-23 12:42:00 +0200

    Sample chat

---
+++ Client/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Run();
+        }
+
+        private void Run()
+        {
+            TcpClient server = new TcpClient("127.0.0.1", 25000);
+
+            NetworkStream ns = server.GetStream();
+            StreamWriter sw = new StreamWriter(ns);
+            StreamReader sr = new StreamReader(ns);
+            sw.AutoFlush = true;
+
+            Thread t = new Thread(PrintSvMessages);
+            t.Start(sr);
+
+            string clientMessage = "";
+
+            do
+            {
+                clientMessage = Console.ReadLine();
+                sw.WriteLine(clientMessage);
+            } while (clientMessage != "quit");
+
+            server.Close();
+        }
+
+        private void PrintSvMessages(object obj)
+        {
+            StreamReader sr = (StreamReader)obj;
+
+            while(true)
+            {
+                string svMsg = sr.ReadLine();
+                string[] svMessage = svMsg.Split(':');
+                switch (svMessage[0])
+                {
+                    case "MESSAGE":
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        Console.WriteLine(svMsg.Substring(svMsg.IndexOf(':')+1));
+                        Console.ResetColor();
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        break;
+                    case "LISTOFUSERS":
+                        int i = 0;
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        foreach (string s in svMsg.Substring(svMsg.IndexOf(':') + 1).Split('%'))
+                        {
+                            Console.WriteLine("Username: " + s);
+                            i++;
+                        }
+                        Console.WriteLine("Total users: " + i);
+                        Console.ResetColor();
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        break;
+                    case "ADMIN":
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+namespace Server
+{
+    class Program
+    {
+        List<StreamWriter> listOfClients;
+        List<Socket> listOfClientsSockets;
+        static void Main(string[] args)
+        {
+            new Program().Run();
+        }
+
+        private void Run()
+        {
+            listOfClients = new List<StreamWriter>();
+            listOfClientsSockets = new List<Socket>();
+            TcpListener server = new TcpListener(IPAddress.Any, 25000);
+            server.Start();
+            
+            
+            while (true)
+            {
+                Socket client = server.AcceptSocket();
+                listOfClientsSockets.Add(client);
+                Thread t = new Thread(ClientProc);
+                t.Start(client);
+            }
+        }
+
+        private void ClientProc(object obj)
+        {
+            Socket client = (Socket)obj;
+
+            NetworkStream ns = new NetworkStream(client);
+            StreamWriter sw = new StreamWriter(ns);
+            StreamReader sr = new StreamReader(ns);
+            sw.AutoFlush = true;
+
+            listOfClients.Add(sw);
+            InformationThread();
+
+            while (true)
+            {
+                Console.WriteLine("Received a message from: " + client.LocalEndPoint);
+                Broadcast("MESSAGE:" + client.LocalEndPoint + ": " + sr.ReadLine(), sw);
+            }
+        }
+
+        private void Broadcast(string msg, StreamWriter sw)
+        {
+            foreach (StreamWriter s in listOfClients)
+            {
+                if (!s.Equals(sw))
+                    s.WriteLine(msg);
+            }
+        }
+
+        private void Broadcast(string msg)
+        {
+            foreach (StreamWriter s in listOfClients)
+            {
+                s.WriteLine(msg);
+            }
+        }
+
+        private void InformationThread()
+        {
+            string finalString = "LISTOFUSERS:";
+
+            foreach (Socket s in listOfClientsSockets)
+            {
+                finalString += s.LocalEndPoint + "%";
+            }
+            Broadcast(finalString.Trim('%'));
+        }
+    }
+}
